7e02124c6a1f613eb93a587c0a438db231d7c0fd
ElPar-coder <jeanparco59@gmail.com>
2024-06-29 18:19:54 +0200

    Update yas_morning.py

--- yas_morning.py
+++ yas_morning.py
@@ -1,33 +1,5 @@
 import streamlit as st
-import requests
-from datetime import datetime, timedelta
-
-# Configuration du bot Telegram
-TELEGRAM_BOT_TOKEN = '7034880568:AAHHO-weYiLbhSF63OusOFoiB-eMDrTqosc'
-TELEGRAM_CHAT_ID = '-1002242000827'
-
-# Fonction pour envoyer un message à Telegram
-def send_message_to_telegram(message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    data = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": message
-    }
-    response = requests.post(url, data=data)
-    return response.json()
-
-# Fonction pour vérifier si l'utilisateur peut pointer aujourd'hui
-def can_user_point_today():
-    last_point_date = st.session_state.get('last_point_date')
-    if last_point_date is None:
-        return True  # Premier pointage de l'utilisateur
-
-    today = datetime.now().date()
-    return last_point_date < today
-
-# Fonction pour enregistrer le pointage de l'utilisateur
-def record_user_point():
-    st.session_state.last_point_date = datetime.now().date()
+from datetime import datetime
 
 # Fonction principale de l'application Streamlit
 def yansmorning():
@@ -38,23 +10,24 @@ def yansmorning():
     st.write(f"**Date du jour :** {today_date} **Il est :** {current_time}")
     st.info('Les réponses seront envoyées au bureau de Enclo sur Telegram.', icon="ℹ️")
     
-    # Affichage du bouton de pointage uniquement si l'utilisateur peut pointer aujourd'hui
-    if can_user_point_today():
+    # Vérifier si le bouton a déjà été cliqué
+    if 'button_clicked' not in st.session_state:
+        st.session_state.button_clicked = False
+    
+    if not st.session_state.button_clicked:
         reponse = st.radio("T'es cuck ?", ("Oui", "Non"))
         if st.button("Valider"):
+            st.session_state.button_clicked = True
             if reponse == "Oui":
                 st.write("Sale cuck 🦆.")
                 st.success(f'Pointage validé à {current_time} !', icon="✅")
-                send_message_to_telegram(f"Il est cuck ce fdp aujourd'hui le {today_date} il a pointé à {current_time}.")
+                # Appeler la fonction pour envoyer un message Telegram ici
             else:
                 st.write("OK j'fais un café ☕️")
                 st.success(f'Pointage validé à {current_time} !', icon="✅")
-                send_message_to_telegram(f"Miracle il n'est pas cuck aujourd'hui le {today_date} il a pointé à {current_time} go café.")
-            
-            # Enregistrement du pointage après validation
-            record_user_point()
+                # Appeler la fonction pour envoyer un message Telegram ici
     else:
-        st.write("T'a déjà pointé aujourd'hui.")
+        st.write("Vous avez déjà pointé aujourd'hui.")
 
 # Exécution de l'application Streamlit
 if __name__ == "__main__":
