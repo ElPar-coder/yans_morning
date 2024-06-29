307fa80df1b69ec492cc3ba08bfacfca727afacf
ElPar-coder <jeanparco59@gmail.com>
2024-06-29 20:15:35 +0200

    Update yas_morning.py

--- yas_morning.py
+++ yas_morning.py
@@ -58,7 +58,6 @@ def yansmorning():
         st.write("Désolé, vous avez déjà pointé aujourd'hui.")
         
     st.sidebar.title("Historique")
-    st.write("Yans à été cuck 30 fois ce mois ci.")
     sselected_date = st.sidebar.date_input("Choisir un jour précis : ", datetime.now())
 
 # Exécution de l'application Streamlit
